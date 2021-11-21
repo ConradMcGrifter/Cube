f6bbe915358c81d0ea5090adc84ea5f82d5dba9a
skf <conradmcgrifter@protonmail.com>
2021-11-21 03:36:18 -0500

    tidy up

--- js/main.js
+++ js/main.js
@@ -1,26 +1,12 @@
 import { zendikarRisingObj, zendikarRising, zendikarRising_page1 } from "./zendikar.js";
 import { cube } from "./cube.js";
 
-// let reducedZendikar = zendikarRising_page1.reduce((acc, card) => {
-//     return {
-//         ...acc,
-//         [card.name]: { ["image"]: card.image, ["cmc"]: card.cmc, ["color"]: card.color },
-//     };
-// }, {});
-
-// let cardName = "Acquisitions Expert";
-
-// console.log(reducedZendikar[cardName]);
-
-// take array of cards in our cube and look for each of those cards by name in the reducedZendikar object
-// if the card matches with a card in the reducedZendikar object --> add the card object to a new array (updated collection)
-// so now instead of just having the names of the cards in our cube, we have a object for each card that contains the img url, cmc, and type
-
 let detailedCube = cube
     .map((card) => {
         if (zendikarRisingObj[card]) {
             return { ...zendikarRisingObj[card], ["name"]: card };
         }
+        // add an if statement here to check kaldheim / AFR , etc... just like the above code
     })
     .filter((card) => {
         if (card !== undefined) {
@@ -43,16 +29,16 @@ let detailedCubeObj = detailedCube.reduce((acc, card) => {
 console.log(detailedCube);
 console.log(detailedCubeObj["Disenchant"]);
 
-// --- testing ---
-function createElement(i) {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    document.body.append(card);
-    card.style.backgroundImage = `url(${detailedCubeObj[i].image})`;
-}
+// --- testing card creation---
+// function createElement(cardName) {
+//     const card = document.createElement("div");
+//     card.classList.add("card");
+//     document.body.append(card);
+//     card.style.backgroundImage = `url(${detailedCubeObj[cardName].image})`;
+// }
 
-for (let i = 0; i < detailedCube.length; i++) {
-    createElement(detailedCube[i].name);
-}
+// for (let i = 0; i < detailedCube.length; i++) {
+//     createElement(detailedCube[i].name);
+// }
 
 // --- end of testing ---
---
+++ todo.md
@@ -0,0 +1,2 @@
+// finish scraping other sets and building arrays
+// add logic to the detailedCube (main.js) that checks the other sets for the card name of each card in the cube. currently it is only checking zendikar cards
